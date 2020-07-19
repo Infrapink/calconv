3ac86751f518de158fbc364a1a316761b6062040
VampireBuddha <cmmccrohan@gmail.com>
2020-07-19 19:52:37 +0100

    Added ADA

--- calconv.py
+++ calconv.py
@@ -36,6 +36,7 @@ import ast_gregorian
 import nex
 import positivist
 import holocene
+import ada
 
 def cons_day_julian_todate():
         """Take the input into the Julian Day box andtojd it into the other date formats"""
@@ -412,6 +413,15 @@ def cons_day_julian_todate():
         holocene_day_ent.insert(0, holocene_date[0])
         holocene_month_ent.insert(0, holocene_date[1])
         holocene_year_ent.insert(0, holocene_date[2])
+
+        # Convert a Julian day to an ADA date
+        ada_date = ada.fromjd(day)
+        ada_day_ent.delete(0, END)
+        ada_month_ent.delete(0, END)
+        ada_year_ent.delete(0, END)
+        ada_day_ent.insert(0, ada_date[0])
+        ada_month_ent.insert(0, ada_date[1])
+        ada_year_ent.insert(0, ada_date[2])
 
 def cons_day_julian_plus():
         day = cons_day_julian_ent.get()
@@ -795,6 +805,14 @@ def holocene_converter():
         cons_day_julian_ent.insert(0, jday)
         cons_day_julian_todate()
 
+def ada_converter():
+        day = int(ada_day_ent.get())
+        month = ada_month_ent.get()
+        year = int(ada_year_ent.get())
+        jday = ada.tojd(day, month, year)
+        cons_day_julian_ent.delete(0, END)
+        cons_day_julian_ent.insert(0, jday)
+        cons_day_julian_todate()
 
 
 root = Tk()
@@ -1294,7 +1312,18 @@ holocene_year_ent = Entry(frame)
 holocene_year_ent.grid(row = 35, column = 2, sticky = W)
 holocene_bttn = Button(frame, text = "Calculate", command = holocene_converter).grid(row = 36, column = 0, columnspan = 3, sticky = W)
 
-
+# ADA calendar                                                                                            
+ada_lbl = Label(frame, text = "ADA").grid(row = 33, column = 3, columnspan = 3, sticky = W)
+ada_day_lbl = Label(frame, text = "Day").grid(row = 34, column = 3, sticky = W)
+ada_day_ent = Entry(frame)
+ada_day_ent.grid(row = 35, column = 3, sticky = W)
+ada_month_lbl = Label(frame, text = "Month").grid(row = 34, column = 4, sticky = W)
+ada_month_ent = Entry(frame)
+ada_month_ent.grid(row = 35, column = 4, sticky = W)
+ada_year_lbl = Label(frame, text = "Year").grid(row = 34, column = 5, sticky = W)
+ada_year_ent = Entry(frame)
+ada_year_ent.grid(row = 35, column = 5, sticky = W)
+ada_bttn = Button(frame, text = "Calculate", command = ada_converter).grid(row = 36, column = 3, columnspan = 3, sticky = W)
 
 root.title("Calendar Converter 0.17.0")
 root.mainloop()
