0a1e187065a246c1522835d697b458a5fa3e0ee6
Chris McCrohan <cmmccrohan@gmail.com>
2020-10-26 19:28:42 +0000

    Add files via upload

--- pax.py
+++ pax.py
@@ -1,88 +1,69 @@
 #!/usr/bin/python
 
 #
-# Convert between the astronomical Gregorian Calendar and Julian Day
+# Convert between the Pax calendar and Julian day
 #
 
 import months
 
-cycle4 = (4 * 365) + 1
-cycle100 = (100 * 365) + 24
 cycle400 = (400 * 365) + 97
+#epoch = 1721059
+epoch = 1721061
 
-epoch = 1721059
+leap_digits_ad = (0,6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96,99)
+leap_digits_bc = (0,94,88,82,76,70,64,58,52,46,40,34,28,22,16,10,4,1)
 
 def tojd(day, month, year):
-
     day = int(day)
-    month = month.title()
+    month = month
     year = int(year)
+    lear = abs(year) % 100 # use to check leap years
     jday = epoch
+    y = 0
 
-    if year > 0:
+    if year >= 0:
         # positive dates
-        y = 0
-        cycles = (year - y) // 400
+        cycles = year // 400
         y += (400 * cycles)
-        jday += (cycles * cycle400)
+        jday += (cycle400 * cycles)
+
         while y < year:
-            if year - y > 400:
-                y += 400
-                jday += cycle400
-            elif year - y > 100:
-                y += 100
-                jday += cycle100
-            elif year - y > 4:
-                y += 4
-                jday += cycle4
-            elif y % 400 == 0:
-                y += 1
-                jday += 366
-            elif y % 100 == 0:
-                y += 1
-                jday += 365
-            elif y % 4 == 0:
-                y += 1
-                jday += 366
+            if y % 400 == 0:
+                jday += 364
+            elif y % 100 in leap_digits_ad:
+                jday += 371
             else:
-                y += 1
-                jday += 365
+                jday += 364
+            y += 1
 
         if year % 400 == 0:
-            m = months.PAX_LEAP
-        elif year % 100 == 0:
+            # not a leap year
             m = months.PAX_NORMAL
-        elif year % 4 == 0:
+        elif year % 100 in leap_digits_ad:
+            # leap year
             m = months.PAX_LEAP
         else:
+            # not a leap year
             m = months.PAX_NORMAL
+
     else:
-        # negative years
-        y = 0
-        cycles = (y - year) // 400
+        # negative dates
+        cycles = abs(year) // 400
         y -= (400 * cycles)
         jday -= (cycles * cycle400)
         
         while y > year:
-            if y - year > 400:
-                y -= 400
-                jday -= cycle400
+            y -= 1
+            if y % 400 == 0:
+                jday -= 364
+            elif abs(y) % 100 in leap_digits_bc:
+                jday -= 371
             else:
-                y -= 1
-                if abs(y) % 400 == 0:
-                    jday -= 366
-                elif abs(y) % 100 == 0:
-                    jday -= 365
-                elif abs(y) % 4 == 0:
-                    jday -= 366
-                else:
-                    jday -= 365
-
-        if abs(year) % 400 == 0:
-            m = months.PAX_LEAP
-        elif abs(year) % 100 == 0:
+                jday -= 364
+        
+        if year % 400 == 0:
             m = months.PAX_NORMAL
-        elif abs(year) % 4 == 0:
+        elif abs(year) % 100 in leap_digits_bc:
             m = months.PAX_LEAP
         else:
             m = months.PAX_NORMAL
@@ -97,56 +78,45 @@ def tojd(day, month, year):
     return jday
 
 def fromjd(jday):
-    """Convert a Julian Day to a date in the Gregorian calendar"""
+    """Convert a Julian day to a date in the Pax calendar."""
     jday = int(jday)
     year = 0
     month = ""
+    day = 0
     nyd = epoch
     curryear = False
-
+    
     if jday >= epoch:
-        # positive date
+        # positive dates
         cycles = (jday - nyd) // cycle400
         year += (400 * cycles)
-        nyd += (cycles * cycle400)
-        while curryear == False:
-            if jday - nyd > cycle400:
-                year += 400
-                nyd += cycle400
-            else:
-                #year += 1
-                if year % 400 == 0:
-                    if jday - nyd <= 366:
-                        curryear = True
-                    else:
-                        nyd += 366
-                        year += 1
-                elif year % 100 == 0:
-                    if jday - nyd <= 365:
-                        curryear = True
-                    else:
-                        nyd += 365
-                        year += 1
-                elif year % 4 == 0:
-                    if jday - nyd <= 366:
-                        curryear = True
-                    else:
-                        nyd += 366
-                        year += 1
-                else:
-                    if jday - nyd <= 365:
-                        curryear = True
-                    else:
-                        nyd += 365
-                        year +=1
+        nyd += (cycle400 * cycles)
         
+        while curryear == False:
+            if year % 400 == 0:
+                if jday - nyd < 364:
+                    curryear = True
+                    break
+                else:
+                    nyd += 364
+            elif year % 100 in leap_digits_ad:
+                if jday - nyd < 371:
+                    curryear = True
+                    break
+                else:
+                    nyd += 371
+            else:
+                if jday - nyd < 364:
+                    curryear = True
+                    break
+                else:
+                    nyd += 364
+            year += 1
+            
         if year % 400 == 0:
-            # leap year
-            m = months.PAX_LEAP
-        elif year % 100 == 0:
             # not a leap year
             m = months.PAX_NORMAL
-        elif year % 4 == 0:
+        elif year % 100 in leap_digits_ad:
             # leap year
             m = months.PAX_LEAP
         else:
@@ -154,33 +124,29 @@ def fromjd(jday):
             m = months.PAX_NORMAL
 
     else:
-        # negative date
+        # negative dates
         cycles = (nyd - jday) // cycle400
         year -= (400 * cycles)
-        nyd -= (cycles * cycle400)
+        nyd -= (cycle400 * cycles)
 
         while nyd > jday:
             year -= 1
             if abs(year) % 400 == 0:
-                nyd -= 366
-            elif abs(year) % 100 == 0:
-                nyd -= 365
-            elif abs(year) % 4 == 0:
-                nyd -= 366
+                nyd -= 364
+            elif abs(year) % 100 in leap_digits_bc:
+                nyd -= 371
             else:
-                nyd -= 365
-           
+                nyd -= 364
+            #year -= 1
+        
         if abs(year) % 400 == 0:
-            # leap year
-            m = months.PAX_LEAP
-        elif abs(year) % 100 == 0:
             # not a leap year
             m = months.PAX_NORMAL
-        elif abs(year) % 4 == 0:
+        elif abs(year) % 100 in leap_digits_bc:
             # leap year
             m = months.PAX_LEAP
         else:
-            # not leap year
+            # not a leap yer
             m = months.PAX_NORMAL
 
     delta = jday - nyd
@@ -193,3 +159,4 @@ def fromjd(jday):
             delta -= m[i]
 
     return (day, month, year)
+
