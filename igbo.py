ff474efd42c64ad6a0499c98d8ddc6ce4d155418
Chris McCrohan <cmmccrohan@gmail.com>
2020-11-08 21:55:41 +0000

    Cleanup

--- igbo.py
+++ igbo.py
@@ -5,32 +5,28 @@
 #
 
 import months
+epoch = 2087607
+m = months.IGBO
 
 def tojd(day, month, year):
     day = int(day)
     month = month
     year = int(year)
-    jday = 2087607
-    m = months.IGBO
+    jday = epoch
     
     if year > 0:
         # positive dates
         jday += (364 * (year - 1))
-        for i in m.keys():
-            if i == month:
-                jday += day - 1 # subtract 1 to account for the fencepost error
-                break
-            else:
-                jday += m[i]
     else:
         # negative dates
-        jday += (364 * year)
-        for i in m.keys():
-            if i == month:
-                jday += day - 1
-                break
-            else:
-                jday += m[i]
+        jday += (365 * year)
+
+    for i in m.keys():
+        if i == month:
+            jday += day - 1
+            break
+        else:
+            jday += m[i]
 
     return jday
 
@@ -39,33 +35,22 @@ def fromjd(jday):
     day = 0
     month = ""
     year = 0
-    m = months.IGBO
 
-    if jday >= 2087607:
+    if jday >= epoch:
         # positive dates
-        delta = jday - 2087607
-        year = (delta // 364) + 1
-        delta %= 364
-        for i in m.keys():
-            if delta < m[i]:
-                month = i
-                day = delta + 1
-                break
-            else:
-                delta -= m[i]
+        year = ((jday - epoch) // 364) + 1
     else:
         # negative dates
-        delta = jday - 2087607
-        year = (abs(delta) // 364) + 1
-        year = 0 - year
-        delta %= 364
+        year = 0 - (((epoch - jday) // 364) + 1)
 
-        for i in m.keys():
-            if delta < m[i]:
-                month = i
-                day = delta + 1
-                break
-            else:
-                delta -= m[i]
+    delta = (jday - epoch) % 364
+
+    for i in m.keys():
+        if delta < m[i]:
+            month = i
+            day = delta + 1
+            break
+        else:
+            delta -= m[i]
 
     return (day, month, year)
