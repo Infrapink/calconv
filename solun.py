548cafd7943871a31def2e3e7659f8c346d3ed1a
Chris McCrohan <cmmccrohan@gmail.com>
2022-06-04 19:24:10 +0100

    Add files via upload

--- solun.py
+++ solun.py
@@ -4,17 +4,20 @@
 
 from fractions import Fraction
 from decimal import Decimal
-from math import floor, ceil
+from math import floor, ceil, cos, acos, pi
 import sunmoon
 import numpy as np
 import gregorian
 from tdiff import DIFF
+import stars
 
 tropical_year = 365 + Fraction(5,24) + Fraction(48,1440) + Fraction(45,86400)
 lunar_month = 29 + Fraction(12,24) + Fraction(44,1440) + Fraction(28,864000)
 solar_term = tropical_year / 12 # time between major solar terms
 year12 = 12 * lunar_month
 year13 = 13 * lunar_month
+r2d = 180 / pi # convert radians to degrees
+d2r = pi / 180 # convert degrees to radians
 
 def udt(jday):
     '''Get the difference between Universal Time and Dynamical Time for a given Julian Day'''
@@ -29,7 +32,7 @@ def udt(jday):
         deltat = Decimal('0.5') * (DIFF[year - 1] + DIFF[year + 1])
     else:
         # approximate value
-        t = (year - 2000) / 100
+        t = Decimal(year - 2000) / 100
         if year < 948:
             deltat = 2177 + (497 * t) + (t * t * Decimal('77.1'))
         elif (year <= 1600) or (year > 2000):
@@ -79,13 +82,57 @@ def truemoon(day, timezone):
     timezone = Fraction(timezone)
     return(floor(conj(day, timezone)))
 
-def sunrise(jday, lon, lat, deltat, ra0, dec0, ra1, dec1, ra2, dec2):
-    jday = floor(jday) - 0.5
+
+def angsep(ra1, dec1, ra2, dec2):
+    '''Determine the angular separation between two bodies, given their right ascensions and declinations'''
+    # this function assumes input angles are in degrees
+    # the answer is given in the zodiacal, not ecliptical, coordinate system
+    ra1 = Decimal(ra1) # right ascension of 1st body
+    dec1 = Decimal(dec1) # declination of 1st body
+    ra2 = Decimal(ra2) # right ascension of 2nd body
+    dec2 = Decimal(dec2) # declination of 2nd body
+
+    cra = cos(d2r * (ra2 - ra1))
+    cdec = cos(d2r * (dec2 - dec1))
+    sigma = r2d * acos(cra + cdes - 1)
+    return sigma
+
+def sunrise(jday, lon, lat):
+    '''Time of sunrise on a given day for a given longitude and latitude'''
+    deltat = udt(int(jday))
+    jday = float(jday) - 0.5 # Julian Day in question
+    lon = float(lon) # observer's longitude
+    lat = float(lat) # observer's latitude
+    ra2000 = 281.29 # https://astronomy.stackexchange.com/questions/35779/ra-and-dec-of-the-sun-at-j2000
+    dec2000 = 0 - 23 - (2/60) - (8.2/3600) # https://astronomy.stackexchange.com/questions/35779/ra-and-dec-of-the-sun-at-j2000
+
+    r = sunmoon.sidereal.solar_riset(jday, lon, lat)
+    return(r[0])
+
+def sunset(jday, lon, lat):
+    '''Time of sunset on a given day for a given longitude and latitude'''
+    deltat = udt(int(jday))
+    jday = int(jday) - 0.5 # Julian Day in question
+    lon = float(lon) # observer's longitude
+    lat = float(lat) # observer's latitude
+    ra2000 = 281.29 # https://astronomy.stackexchange.com/questions/35779/ra-and-dec-of-the-sun-at-j2000
+    dec2000 = 0	- 23 - (2/60) -	(8.2/3600) # https://astronomy.stackexchange.com/questions/35779/ra-and-dec-of-the-sun-at-j2000
+    
+    s =	sunmoon.sidereal.solar_riset(jday, lon, lat)
+    return(s[1])
+
+def starrise(jday, lon, lat, ra2000, dec2000, distance, rv, deltara, deltadec):
+    '''Time of a star rising for a given longitude and latitude'''
+    deltat = udt(int(jday))
+    jday = int(jday) - 0.5
     lon = float(lon)
     lat = float(lat)
-    deltat = float(deltat)
-    yesterday = (float(ra0), float(dec0))
-    today = (float(ra1), float(dec1))
-    tomorrow = (float(ra2), float(dec2))
+    ra2000 = float(ra2000)
+    dec2000 = float(dec2000)
+    distance = float(distance)
+    rv = float(rv)
+    deltara = float(deltara)
+    deltadec = float(deltadec)
 
-    return sunmoon.sidereal.riset(jday, lon, lat, deltat, yesterday, today, tomorrow, 0)
+    s = sunmoon.sidereal.stellar_riset(jday, lon, lat, deltat, ra2000, dec2000, distance, rv, deltara, deltadec)
+    return(s[0])
